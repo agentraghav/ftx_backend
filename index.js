712d0ad2619fb0631c3173d35b8a1aa16bc6f86c
raghvendra <raghvendrarathore2@gmail.com>
2021-12-03 14:19:25 +0530

    login register api setup using jwt

--- index.js
+++ index.js
@@ -1,6 +1,20 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+require('dotenv').config();
 const app = express();
-
-app.listen(1337, () => {
-  console.log('server started');
-});
+app.use(express.json());
+app.use(cors());
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`The server has started on port: ${PORT}`));
+mongoose.connect(
+  process.env.MONGODB_URI,
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  },
+  err => {
+    if (err) throw err;
+    console.log('MongoDB connection established');
+  },
+);
