50cdc64dca322dacfbf76ccb8ee8f8a37550fd89
technophile22 <raghavmangal22@gmail.com>
2021-12-04 01:02:45 +0530

    get and post order api created

--- index.js
+++ index.js
@@ -5,16 +5,31 @@ require('dotenv').config();
 const app = express();
 app.use(express.json());
 app.use(cors());
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect(process.env.MONGODB_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+
+    console.log('MongoDB connected...');
+  } catch (err) {
+    console.log(err.message);
+    //Exit process with failure
+    process.exit(1);
+  }
+};
+
+connectDB();
+
+//Init Middleware
+app.use(express.json({extended: false}));
+
+//Testing route
+app.get('/', (req, res) => res.send('API Running'));
+
+app.use('/api/order', require('./src/routes/order'));
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`The server has started on port: ${PORT}`));
-mongoose.connect(
-  process.env.MONGODB_URI,
-  {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  },
-  err => {
-    if (err) throw err;
-    console.log('MongoDB connection established');
-  },
-);
